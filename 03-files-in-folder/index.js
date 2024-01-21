b8e64bf5c0984e66b218f807ad3e420df9260f65
Valeriya2511 <finsternis.ru@mail.ru>
2024-01-21 22:12:23 +0600

    feat: add solution 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathFiles = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathFiles, (err, files) => {
+    if (err) throw err;
+    for(let file of files) {
+        const infoFile = path.parse(file);
+
+        fs.stat(path.join(pathFiles, infoFile.base), (err, stats) => {
+            if (err) throw err;
+            if(stats.isDirectory()){
+                return;
+            }
+            console.log(`${infoFile.name} - ${infoFile.ext.replace('.', '')} - ${stats.size / 1000}KB`)
+        })
+    }
+});
